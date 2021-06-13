5c697c6ebd2c4a4d57e9fe976ed7cf68d2edde6b
Jan Bronicki <janbronicki@gmail.com>
2021-06-13 23:58:26 +0200

    Added 2 layouts with buttons and timers

---
+++ SportApp/app/src/main/java/com/example/sportapp/ExerciseActivity.java
@@ -0,0 +1,10 @@
+package com.example.sportapp;
+
+public class ExerciseActivity extends MainActivity implements View.OnClickListener {
+    protected void onCreate() {
+
+    }
+    protected void onClick(View v) {
+        finish();
+    }
+}
